158f305c023d36ec80f37609679793b71beb5b95
ShimKiYoung <shim8934@gmail.com>
2021-12-17 22:14:51 +0900

    초기 비밀번호 확인 스크립트 alias zshrc로 설정하도록 변경

--- environment/8.set_PWD_Alias.sh
+++ environment/8.set_PWD_Alias.sh
@@ -1,25 +1,25 @@
 #/bin/bash
 
 
-if [[ `grep "jen_PWD" ~/.bashrc` ]];
+if [[ `grep "jen_PWD" ~/.zshrc` ]];
 then
   echo 'alias jen_PWD exist';
 else
   echo 'alias jen_PWD not exist / alias jen_PWD will be inserted';
-  echo "alias jen_PWD='kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo'" >> ~/.bashrc
+  echo "alias jen_PWD='kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo'" >> ~/.zshrc
 fi
 echo ''
 
-if [[ `grep "argo_PWD" ~/.bashrc` ]];
+if [[ `grep "argo_PWD" ~/.zshrc` ]];
 then
   echo 'alias argo_PWD exist';
 else
   echo 'alias argo_PWD not exist / alias argo_PWD will be inserted';
-  echo "alias argo_PWD='kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}"|base64 -d'" >> ~/.bashrc
+  echo "alias argo_PWD='kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}"|base64 -d'" >> ~/.zshrc
 fi
 echo 'Setting Alias for PWD Finished'
 echo ''
 
-echo 'source ~/.bashrc Finished! Check with jen_PWD & argo_PWD'
-source ~/.bashrc
+echo 'source ~/.zshrc Finished! Check with jen_PWD & argo_PWD'
+source ~/.zshrc
 echo ''
